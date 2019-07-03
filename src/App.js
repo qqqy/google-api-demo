5c5c3e0653283c5aaaa0f0417f56c340c17a189d
qqqy <frogasaurus@hotmail.com>
2019-07-03 09:35:44 -0600

    demo code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper , Marker , InfoWindow } from 'google-maps-react';
 
-function App() {
-  return (
-    <>We in it now boi</>
-  );
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+class MapContainer extends Component {
+
+  state = {
+    showingInfoWindow: false ,
+    activeMarker: {},
+    selectedPlace: {}
+  }
+
+  onMarkerClick = (props, marker , e) => {
+    this.setState({
+      showingInfoWindow: true,
+      selectedPlace: props,
+      activeMarker: marker
+    })
+  }
+
+  onClose = (props) => {
+    if(this.state.showingInfoWindow){
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+  
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      >
+                <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+})(MapContainer);
